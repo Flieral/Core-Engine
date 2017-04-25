bdb5c9b9e4c956ec6920248eaa259df260415cf6
MrWooJ <hurricanc@gmail.com>
2017-04-25 12:28:50 +0430

    Implement User Interaction Tracking to Raccoon Recom-Engine

--- common/models/interaction.js
+++ common/models/interaction.js
@@ -1,6 +1,11 @@
 var utility = require('../../public/utility')
-  
+const raccoon = require('raccoon')
+
 module.exports = function (interaction) {
+
+  raccoon.config.nearestNeighbors = 5
+  raccoon.config.className = 'content'
+  raccoon.config.numOfRecsStore = 30
 
   interaction.generateUserHashId = function (cb) {
     var hashId = utility.generateUniqueHashID();
@@ -21,4 +26,72 @@ module.exports = function (interaction) {
       type: 'string'
     }
   })
+
+  interaction.inputReactions = function (userId, contentId, interactionType, cb) {
+    var whiteList = ['liked', 'disliked', 'unliked', 'undisliked']
+    if (!utility.inputPresence(interactionType, whiteList))
+      return cb(new Error('White List Error! Allowed Parameters: ' + whiteList.toString()), null)
+    switch (interactionType) {
+      case 'liked':
+        raccoon.liked(userId, contentId).then(() => {
+          return cb(null, '[liked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+        })
+        break
+      case 'disliked':
+        raccoon.disliked(userId, contentId).then(() => {
+          return cb(null, '[disliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+        })
+        break
+      case 'unliked':
+        raccoon.unliked(userId, contentId).then(() => {
+          return cb(null, '[unliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+        })
+        break
+      case 'undisliked':
+        raccoon.undisliked(userId, contentId).then(() => {
+          return cb(null, '[undisliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+        })
+        break
+    }
+  }
+
+  interaction.remoteMethod('inputReactions', {
+    accepts: [{
+        arg: 'userId',
+        type: 'string',
+        required: true,
+        http: {
+          source: 'query'
+        }
+      },
+      {
+        arg: 'contentId',
+        type: 'string',
+        required: true,
+        http: {
+          source: 'query'
+        }
+      },
+      {
+        arg: 'interactionType',
+        type: 'string',
+        required: true,
+        http: {
+          source: 'query'
+        }
+      }
+    ],
+    description: 'set input reaction for this particualr user',
+    http: {
+      path: '/inputReactions',
+      verb: 'POST',
+      status: 200,
+      errorStatus: 400
+    },
+    returns: {
+      arg: 'response',
+      type: 'string'
+    }
+  })
+
 }
