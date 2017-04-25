5e1833d480b6b366eb0469708e30f8d754fc6278
MrWooJ <hurricanc@gmail.com>
2017-04-25 11:34:00 +0430

    Modify Core `Server.js` in Order to Enable Authentication ACL

--- server/server.js
+++ server/server.js
@@ -1,29 +1,38 @@
-'use strict';
+'use strict'
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+var loopback = require('loopback')
+var boot = require('loopback-boot')
+var path = require('path')
+var bodyParser = require('body-parser')
 
-var app = module.exports = loopback();
+var app = module.exports = loopback()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(loopback.token({
+    model: app.models.accessToken,
+    currentUserLiteral: 'me'
+}))
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    app.emit('started')
+    var baseUrl = app.get('url').replace(/\/$/, '')
+    console.log('Web server listening at: %s', baseUrl)
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      var explorerPath = app.get('loopback-component-explorer').mountPath
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath)
     }
-  });
-};
+  })
+}
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+  if (err) throw err
 
   // start the server if `$ node server.js`
   if (require.main === module)
-    app.start();
-});
+    app.start()
+})
