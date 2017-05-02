5f74580ec0d38589b8567004dc4567ae95f2f605
MrWooJ <hurricanc@gmail.com>
2017-05-02 12:33:10 +0430

    Authorizing Publisher and Application and Response Placements' Data

--- common/models/content-manager.js
+++ common/models/content-manager.js
@@ -1,3 +1,54 @@
-module.exports = function(contentManager) {
+var utility = require('../../public/utility')
+var requestHandler = require('../../public/requestHandler')
+var config = require('../../server/config')
+
+module.exports = function (contentManager) {
+
+  var baseURL = 'http://' + config.publisherService.server + ':' + config.publisherService.port + '/api/'
+
+  contentManager.recommendFor = function (publisherHashId, applicationHashId, cb) {
+    var url = utility.wrapAccessToken(baseURL + '/clients/' + publisherHashId + '/applications/' + applicationHashId, app.publisherAccessToken)
+    requestHandler.getRequest(url, function (err, applicationResponse) {
+      if (err)
+        return cb(err, null)
+      url = utility.wrapAccessToken(baseURL + '/applications/' + applicationHashId + '/placements', app.publisherAccessToken)
+      requestHandler.getRequest(url, function (err, placementsResponse) {
+        if (err)
+          return cb(err, null)
+        return cb (null, placementsResponse)
+      })
+    })
+  }
+
+  contentManager.remoteMethod('authorization', {
+    accepts: [{
+        arg: 'publisherHashId',
+        type: 'string',
+        required: true,
+        http: {
+          source: 'query'
+        }
+      },
+      {
+        arg: 'applicationHashId',
+        type: 'string',
+        required: true,
+        http: {
+          source: 'query'
+        }
+      }
+    ],
+    description: 'returns status of authorizing publisher and its application and placements of that patrticular applcation',
+    http: {
+      path: '/authorization',
+      verb: 'POST',
+      status: 200,
+      errorStatus: 400
+    },
+    returns: {
+      arg: 'response',
+      type: 'object'
+    }
+  })
 
 }
