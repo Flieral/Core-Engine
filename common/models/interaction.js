fbf7fab18096be7e1e9a2d6d9a3e5ae0c7a91cc4
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-05-29 14:19:22 +0430

    Do Some Code Enhancement in Callback Methods

--- common/models/interaction.js
+++ common/models/interaction.js
@@ -11,7 +11,7 @@ module.exports = function (interaction) {
 
   interaction.generateUserHashId = function (cb) {
     var hashId = utility.generateUniqueHashID();
-    cb(null, hashId)
+    cb(hashId)
   }
 
   interaction.remoteMethod('generateUserHashId', {
@@ -32,36 +32,36 @@ module.exports = function (interaction) {
   interaction.inputReactions = function (userId, contentId, interactionType, cb) {
     var whiteList = ['liked', 'disliked', 'unliked', 'undisliked', 'reported', 'shared']
     if (!utility.inputPresence(interactionType, whiteList))
-      return cb(new Error('White List Error! Allowed Parameters: ' + whiteList.toString()), null)
+      return cb(new Error('White List Error! Allowed Parameters: ' + whiteList.toString()))
     switch (interactionType) {
       case 'liked':
         raccoon.liked(userId, contentId).then(() => {
-          return cb(null, '[liked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+          return cb('[liked] Successful: Content (' + contentId + ') By User (' + userId + ')')
         })
         break
       case 'disliked':
         raccoon.disliked(userId, contentId).then(() => {
-          return cb(null, '[disliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+          return cb('[disliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
         })
         break
       case 'unliked':
         raccoon.unliked(userId, contentId).then(() => {
-          return cb(null, '[unliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+          return cb('[unliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
         })
         break
       case 'undisliked':
         raccoon.undisliked(userId, contentId).then(() => {
-          return cb(null, '[undisliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
+          return cb('[undisliked] Successful: Content (' + contentId + ') By User (' + userId + ')')
         })
         break
       case 'reported':
         raccoon.reported(userId, contentId).then(() => {
-          return cb(null, '[reported] Successful: Content (' + contentId + ') By User (' + userId + ')')
+          return cb('[reported] Successful: Content (' + contentId + ') By User (' + userId + ')')
         })
         break
       case 'shared':
         raccoon.shared(userId, contentId).then(() => {
-          return cb(null, '[shared] Successful: Content (' + contentId + ') By User (' + userId + ')')
+          return cb('[shared] Successful: Content (' + contentId + ') By User (' + userId + ')')
         })
         break
     }
@@ -108,7 +108,7 @@ module.exports = function (interaction) {
 
   interaction.recommendFor = function (userId, numberOfRecs, cb) {
     raccoon.recommendFor(userId, numberOfRecs).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -145,7 +145,7 @@ module.exports = function (interaction) {
 
   interaction.mostSimilarUsers = function (userId, cb) {
     raccoon.mostSimilarUsers(userId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -173,7 +173,7 @@ module.exports = function (interaction) {
 
   interaction.leastSimilarUsers = function (userId, cb) {
     raccoon.leastSimilarUsers(userId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -201,7 +201,7 @@ module.exports = function (interaction) {
 
   interaction.bestRated = function (cb) {
     raccoon.bestRated().then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -222,7 +222,7 @@ module.exports = function (interaction) {
 
   interaction.worstRated = function (cb) {
     raccoon.worstRated().then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -243,7 +243,7 @@ module.exports = function (interaction) {
 
   interaction.mostLiked = function (cb) {
     raccoon.mostLiked().then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -264,7 +264,7 @@ module.exports = function (interaction) {
 
   interaction.mostDisliked = function (cb) {
     raccoon.mostDisliked().then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -285,7 +285,7 @@ module.exports = function (interaction) {
 
   interaction.mostReported = function (cb) {
     raccoon.mostReported().then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -306,7 +306,7 @@ module.exports = function (interaction) {
   
   interaction.mostShared = function (cb) {
     raccoon.mostShared().then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -327,7 +327,7 @@ module.exports = function (interaction) {
 
   interaction.likedBy = function (contentId, cb) {
     raccoon.likedBy(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -355,7 +355,7 @@ module.exports = function (interaction) {
 
   interaction.likedCount = function (contentId, cb) {
     raccoon.likedCount(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -383,7 +383,7 @@ module.exports = function (interaction) {
 
   interaction.dislikedBy = function (contentId, cb) {
     raccoon.dislikedBy(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -411,7 +411,7 @@ module.exports = function (interaction) {
 
   interaction.dislikedCount = function (contentId, cb) {
     raccoon.dislikedCount(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -439,7 +439,7 @@ module.exports = function (interaction) {
 
   interaction.reportedBy = function (contentId, cb) {
     raccoon.reportedBy(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -467,7 +467,7 @@ module.exports = function (interaction) {
 
   interaction.reportedCount = function (contentId, cb) {
     raccoon.reportedCount(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -495,7 +495,7 @@ module.exports = function (interaction) {
   
   interaction.sharedBy = function (contentId, cb) {
     raccoon.sharedBy(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -523,7 +523,7 @@ module.exports = function (interaction) {
 
   interaction.sharedCount = function (contentId, cb) {
     raccoon.sharedCount(contentId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -551,7 +551,7 @@ module.exports = function (interaction) {
   
   interaction.allLikedFor = function (userId, cb) {
     raccoon.allLikedFor(UserId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -579,7 +579,7 @@ module.exports = function (interaction) {
 
   interaction.allDislikedFor = function (userId, cb) {
     raccoon.allDislikedFor(UserId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -607,7 +607,7 @@ module.exports = function (interaction) {
 
   interaction.allReportedFor = function (userId, cb) {
     raccoon.allReportedFor(UserId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -635,7 +635,7 @@ module.exports = function (interaction) {
 
   interaction.allSharedFor = function (userId, cb) {
     raccoon.allSharedFor(UserId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -663,7 +663,7 @@ module.exports = function (interaction) {
 
   interaction.allWatchedFor = function (userId, cb) {
     raccoon.allWatchedFor(UserId).then((results) => {
-      return cb(null, results)
+      return cb(results)
     })
   }
 
@@ -699,8 +699,8 @@ module.exports = function (interaction) {
 
     interaction.replaceOrCreate(model, function (err, result) {
       if (err)
-        throw err
-      cb(null, result)
+        cb(err)
+      cb(result)
     })
   }
 
@@ -737,8 +737,8 @@ module.exports = function (interaction) {
   interaction.loadInformation = function (userId, cb) {
     interaction.findById(userId, function (err, result) {
       if (err)
-        throw err
-      cb(null, result)
+        cb(err)
+      cb(result)
     })
   }
 
